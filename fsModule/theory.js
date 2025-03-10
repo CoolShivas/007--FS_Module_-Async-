179ec858294590b0418e9e5fcd01bb7513f97721
CoolShivas <shivas2710cool00@gmail.com>
2025-03-10 14:47:48 +0530

    Starting of Node.js FS Module Async

---
+++ fsModule/theory.js
@@ -0,0 +1,69 @@
+// /**
+//  * 
+//  * * * // // How to make a folder using Node.js ?
+
+// // With the help of following commands you can make a folder.
+
+// // Step.1) Open the VS Code Terminal.
+
+// // Step.2) Then write the command mkdir fsModule and then hit enter.
+
+// // Step.3) Then the folder is created named as fsModule.
+
+// // Step.4) If you want to move enter then write the command cd fsModule then you can use it.
+//  * 
+//  * 
+//  * 
+//  * 
+//  */
+
+
+
+/***
+ * 
+ * * * * Theory :- 
+ * 
+ * * * * * * FS Module :-
+ * 
+ * 
+ * FS Module - Node.js :-
+ * 
+        The fs (File System) module in Node.js is a core module that allows you 
+        to work with the file system, enabling you to read, write, update, delete, 
+        and watch files.
+
+        
+        Let’s see how to URUD operation in a Synchronous way.
+
+
+ //////// ------------------------------------------------------------------------------------------------------------------
+
+ 
+ * 
+ * 
+ * * *  FS Module – CRUD Operations :- 
+ * * * * * Theory of fs-Module Asynchronous :-
+ * 
+        1.) Create (fs.writeFile)
+        • Writes content to the file. If the file does not exist, it creates one.
+ 
+        2.) Read (fs.readFile)
+        • Reads the content of the file asynchronously and logs it.
+ 
+        3.) Update (fs.appendFile)
+        • Appends new content to the file without overwriting the existing content.
+ 
+        4.) Delete (fs.unlink)
+        • Deletes the file completely.
+ 
+
+ //////// ------------------------------------------------------------------------------------------------------------------
+
+ 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
